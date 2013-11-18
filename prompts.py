b81cc290c85a3a9cfa4ec1d85b4b01a77c324f5f
Peter Becich <peterbecich@gmail.com>
2013-11-18 02:03:28 -0800

    prompts.py

---
+++ prompts.py
@@ -0,0 +1,31 @@
+# prompts
+
+mainMenu = "
+  1) Database Setup
+  2) Translation
+  0) Exit
+  Enter choice:
+"
+
+setupChoices = "
+  1) Insert English word and phrases
+  2) Insert Kangi characters
+  3) Insert Japanese words
+  4) Insert Wasei-Eigo expression (also a Japanese word)
+  ------Translation links--------
+  5) Insert English word sequence -- Japanese word sequence link
+  6) Tag Japanese word with a descriptive English phrase
+  0) Go back
+  Enter choice (default 0):
+"
+
+
+insertWordFrom = "
+  1) Insert %s words and phrases by prompt;
+     distinct words, phrases separated by comma;
+     phrases joined by space or hyphen
+  2) Import %s words from %s.csv
+  0) Go back
+  Enter choice (default 0):
+"
+
