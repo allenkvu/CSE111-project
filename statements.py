18c15756171c2b4ab708a61453981e4cf48701d7
zakura15 <dragonzakura15@gmail.com>
2013-11-18 01:06:03 -0800

    updated the statements.py

--- statements.py
+++ statements.py
@@ -82,6 +82,7 @@ insertJapaneseWordEnglishLink = "insert into japaneseEnglishWordLink (
 # Queries Through Links
 ######################
 
+
 # English word to Kana sequence
 getKanaFromEnglishWord = "SELECT kns_hiragana, kns_katakana, kns_romaji (
 FROM kanaSystem
@@ -94,8 +95,17 @@ ORDER BY japaneseWordKanaLink.jwkn_kanaOrder;"
 
 # English sequence to Kana sequence
 # Peter will do this
-getKanaFromEnglishPhrase = " SELECT 
-
+# this could be wrong my head is not clear enough.
+getKanaFromEnglishPhrase = " SELECT kns_hiragana, kns_katakana, kns_romaji (
+FROM kanaSystem
+INNER JOIN japaneseWordKanaLink
+ON kanaSystem.kns_kanaID = japaneseWordKanaLink.jwkn_kanaID
+INNER JOIN japaneseEnglishWordLink
+ON japaneseWordKanaLink.jwkn_japaneseWordID = japaneseEnglishWordLink.jewl_japaneseWordID
+INNER JOIN englishPhrase
+ON japaneseEnglishWordLink.jewl_englishWordID = englishPhrase.ep_ID
+WHERE japaneseEnglishWordLink.jewl_englishWordID = ?
+ORDER BY japaneseWordKanaLink.jwkn_kanaOrder;"
 "
 
 # Japanese word to English sequence
